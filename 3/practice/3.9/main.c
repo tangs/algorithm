245c6ac8352e15fe7936fd916008679834758458
tangs <tangsong1988kaka@gmail.com>
2019-01-07 12:22:46 +0800

    Print digital distribution.

--- 3/practice/3.9/main.c
+++ 3/practice/3.9/main.c
@@ -3,31 +3,36 @@
 
 #include "Number.h"
 
-void PrintZore(int times) {
-    for (int i = 0; i < times; ++i)
-        printf("%d", 0);
+int PrintZore(int times, char *str, int len) {
+    for (int i = 0; i < times; ++i) {
+        snprintf(str++, len, "%d", 0);
+    }
+    return times;
 }
 
-void PrintNumber(Number num) {
+int NumberToString(Number num, char *str, int len) {
     Position p = First(num);
     Position prev = NULL;
     int digit = -1;
+    char *pStr = str;
     char tmp[NUMBER_OVERFLOWBITS];
     memset(tmp, '0', NUMBER_OVERFLOWBITS - 1);
     while (p) {
         int curDigit = Digit(p);
-        PrintZore((digit - curDigit) * NUMBER_OVERFLOWBITS);
-        int len = snprintf(tmp, NUMBER_OVERFLOWBITS, "%d", Value(p));
+         if (digit != -1)
+            pStr += PrintZore((digit - curDigit) * NUMBER_OVERFLOWBITS, pStr, len - (pStr - str));
+        int count = snprintf(tmp, NUMBER_OVERFLOWBITS, "%d", Value(p));
         if (digit != -1)
-            PrintZore(NUMBER_OVERFLOWBITS - len - 1);
-        printf("%s", tmp);
+            pStr += PrintZore(NUMBER_OVERFLOWBITS - count - 1, pStr, len - (pStr - str));
+        pStr += snprintf(pStr, len - (pStr - str), "%s", tmp);
         digit = curDigit - 1;
         prev = p;
         p = Advance(p);
     }
     if (prev)
-        PrintZore(Digit(prev) - 1);
-    printf("\n");
+        pStr += PrintZore(Digit(prev) - 1, pStr, len - (pStr - str));
+    *pStr = 0;
+    return pStr - str;
 }
 
 int main() {
@@ -44,7 +49,20 @@ int main() {
         ret = Mut(num1, ret);
         DisposeNumber(tmp);
     }
-    PrintNumber(ret);
+    // PrintNumber(ret);
+    const len = 1024 * 10;
+    char str[len];
+    int cnt = NumberToString(ret, str, len);
+    int cnts[10];
+    memset(cnts, 0, sizeof(int) * 10);
+    for (int i = 0; i < cnt; ++i) {
+        ++cnts[str[i] - '0'];
+    }
+    printf("%s\n", str);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d:%d\n", i, cnts[i]);
+    }
+    printf("total:%d\n", cnt);
 
     DisposeNumber(num1);
     // DisposeNumber(num2);
