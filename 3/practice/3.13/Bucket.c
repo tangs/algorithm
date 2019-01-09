55347d2576f42dfc3b56e642709a406d1ea96e2c
tangs <tangsong1988kaka@gmail.com>
2019-01-09 18:22:33 +0800

    Fixed bucket sort.

--- 3/practice/3.13/Bucket.c
+++ 3/practice/3.13/Bucket.c
@@ -3,42 +3,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-void Sort_Bucket(List l, int base, int times) {
-    List *buckets = malloc(base * sizeof(List));
-    for (int i = 0; i < base; ++i)
-        buckets[i] = CreateList();
-    
-    Position p = First(l);
-    while (!IsHeader(p, l)) {
-        ElementType element = Retrieve(p);
-        int idx = element % base;
-        Append(element, buckets[idx]);
-        p = Advance(p, l);
-    }
-
-    int num = base;
-    for (int i = 1; i < times; ++i) {
-        for (int j = 0; j < base; ++j) {
-            List bucket = buckets[j];
-            Position p = First(bucket);
-            Position endP = NULL;
-            while (!IsHeader(p, bucket) && endP != p) {
-                ElementType element = Retrieve(p);
-                int idx = (element / num) % base;
-                p = Delete(p, bucket);
-                Append(element, buckets[idx]);
-
-                if (!endP && idx == j) {
-                    endP = Last(buckets[idx]);
-                }
-                p = Advance(p, bucket);
-            }
-        }
-        num *= base;
-    }
-
+void BucketsToList(List* buckets, int num, List l) {
     MakeEmpty(l);
-    for (int i = 0; i < base; ++i) {
+    for (int i = 0; i < num; ++i) {
         List bucket = buckets[i];
         Position p = First(bucket);
         while (!IsHeader(p, bucket)) {
@@ -46,6 +13,26 @@ void Sort_Bucket(List l, int base, int times) {
             Append(element, l);
             p = Advance(p, bucket);
         }
+        MakeEmpty(bucket);
+    }
+}
+
+void Sort_Bucket(List l, int base, int times) {
+    List *buckets = malloc(base * sizeof(List));
+    for (int i = 0; i < base; ++i)
+        buckets[i] = CreateList();
+    
+    int num = base;
+    for (int i = 0; i < times; ++i) {
+        Position p = First(l);
+        while (!IsHeader(p, l)) {
+            ElementType element = Retrieve(p);
+            int idx = (element / num) % base;
+            Append(element, buckets[idx]);
+            p = Advance(p, l);
+        }
+        BucketsToList(buckets, base, l);
+        num *= base;
     }
 
     for (int i = 0; i < base; ++i)
