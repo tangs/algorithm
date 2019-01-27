a51ab1c8f2ca7e4fd786708006bc17af272e9c28
tangs <tangsong1988kaka@gmail.com>
2019-01-27 22:04:54 +0800

    Implement Tree.Inser().

--- 4/pracrice/4.3.1/Tree.c
+++ 4/pracrice/4.3.1/Tree.c
@@ -30,19 +30,47 @@ Tree_Pos Tree_Find(ElementType x, SearchTree t) {
 Tree_Pos Tree_FindMin(SearchTree t) {
     if (t == NULL) 
         return NULL;
-    if (t->left)
+    if (t->left != NULL)
         return Tree_FindMin(t->left);
     return t;
 }
 
 Tree_Pos Tree_FindMax(SearchTree t) {
-    if (t == NULL)
-        return NULL;
-    if (t->right) {
-        return Tree_FindMin(t->right);
+    if (t != NULL)
+        while (t->right != NULL)
+            t = t->right;
     return t;
 }
 
-SearchTree Insert(ElementType x, SearchTree t);
+SearchTree Insert(ElementType x, SearchTree t) {
+    if (t == NULL)
+        return NULL;
+    if (x == t->element)
+        return t;
+    if (x < t->element)
+        if (t->left != NULL)
+            return Insert(x, t->left);
+        else {
+            SearchTree tree = malloc(sizeof(struct Tree_Node));
+            tree->left = NULL;
+            tree->right = NULL;
+            tree->element = x;
+            t->left = tree;
+            return tree;
+        }
+    if (x > t->element)
+        if (t->right != NULL)
+            return Insert(x, t->right);
+        else {
+            SearchTree tree = malloc(sizeof(struct Tree_Node));
+            tree->right = NULL;
+            tree->right = NULL;
+            tree->element = x;
+            t->right = tree;
+            return tree;
+        }
+    return t;
+}
+
 SearchTree Delete(ElementType x, SearchTree t);
 ElementType Retrieve(Tree_Pos p);
