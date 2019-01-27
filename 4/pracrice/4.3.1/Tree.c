8b44505a8935d4d53f9025462fd16a388d45fa87
tangs <tangsong1988kaka@gmail.com>
2019-01-27 21:49:18 +0800

    Update tree.

---
+++ 4/pracrice/4.3.1/Tree.c
@@ -0,0 +1,52 @@
+#include "Tree.h"
+
+#include <stdlib.h>
+
+struct Tree_Node {
+    ElementType element;
+    SearchTree left;
+    SearchTree right;
+};
+
+SearchTree Tree_MakeEmpty(SearchTree t) {
+    if (t != NULL) {
+        Tree_MakeEmpty(t->left);
+        Tree_MakeEmpty(t->right);
+        free(t);
+    }
+    return NULL;
+}
+
+Tree_Pos Tree_Find(ElementType x, SearchTree t) {
+    if (t == NULL) {
+        return NULL;
+    }
+    if (t == x) {
+        return t;
+    } else if (x < t->element) {
+        return Tree_Find(x, t->left);
+    }
+    return Tree_Find(x, t->right);
+}
+
+Tree_Pos Tree_FindMin(SearchTree t) {
+    if (t == NULL) {
+        return NULL;
+    } else if (t->left) {
+        return Tree_FindMin(t->left);
+    }
+    return t;
+}
+
+Tree_Pos Tree_FindMax(SearchTree t) {
+    if (t == NULL) {
+        return NULL;
+    } else if (t->right) {
+        return Tree_FindMin(t->right);
+    }
+    return t;
+}
+
+SearchTree Insert(ElementType x, SearchTree t);
+SearchTree Delete(ElementType x, SearchTree t);
+ElementType Retrieve(Tree_Pos p);
--- 4/pracrice/4.3.1/Tree.h
+++ 4/pracrice/4.3.1/Tree.h
@@ -1,11 +1,18 @@
 #ifndef _Tree_h
 #define _Tree_h
 
+#include "Def.h"
+
 struct Tree_Node;
 typedef struct Tree_Node *Tree_Pos;
 typedef struct Tree_Node *SearchTree;
 
 SearchTree Tree_MakeEmpty(SearchTree t);
-Position Tree_Find(ElementType x, SearchTree t);
+Tree_Pos Tree_Find(ElementType x, SearchTree t);
+Tree_Pos Tree_FindMin(SearchTree t);
+Tree_Pos Tree_FindMax(SearchTree t);
+SearchTree Insert(ElementType x, SearchTree t);
+SearchTree Delete(ElementType x, SearchTree t);
+ElementType Retrieve(Tree_Pos p);
 
 #endif // _Tree_h
