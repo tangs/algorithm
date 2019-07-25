a5d681b5baccbb005331723b7b6b6292779e81e9
tangs <tangsong1988kaka@gmail.com>
2019-07-25 13:59:19 +0800

    optimise quick sort.

--- 7/7.7/1.c
+++ 7/7.7/1.c
@@ -1,28 +1,42 @@
 #include "../common/tools.h"
 
-void swap(int *a, int *b) {
+#define Cutoff 3
+
+void Swap(int* a, int* b) {
     int c = *a;
     *a = *b;
     *b = c;
+}
+
+void InsertionSort(int *data, int len) {
+    for (int i = 1; i < len; ++i) {
+        int tmp = data[i];
+        int j;
+        for (j = i - 1; j >= 0 && data[j] > tmp; --j)
+            data[j + 1] = data[j];
+        data[j + 1] = tmp;
+    }
 }
 
 int Median3(int *data, int n, int s, int e) {
     // ShowArr(data, n);
     int mid = (s + e) / 2;
     if (data[s] > data[mid])
-        swap(&data[s], &data[mid]);
+        Swap(&data[s], &data[mid]);
     if (data[s] > data[e])
-        swap(&data[s], &data[e]);
+        Swap(&data[s], &data[e]);
     if (data[mid] > data[e])
-        swap(&data[mid], &data[e]);
-    swap(&data[mid], &data[e - 1]);
+        Swap(&data[mid], &data[e]);
+    Swap(&data[mid], &data[e - 1]);
     // ShowArr(data, n);
     return data[e - 1];
 }
 
 void QSort(int *data, int n, int s, int e) {
-    if (s >= e)
+    if (s + Cutoff > e) {
+        InsertionSort(data + s, e - s + 1);
         return;
+    }
     int pivot = Median3(data, n, s, e);
     int i = s;
     int j = e - 1;
@@ -30,12 +44,12 @@ void QSort(int *data, int n, int s, int e) {
         while(data[++i] < pivot);
         while(data[--j] > pivot);
         if (i < j) {
-            swap(&data[i], &data[j]);
+            Swap(&data[i], &data[j]);
         } else {
             break;
         }
     }
-    swap(&data[i], &data[e - 1]);
+    Swap(&data[i], &data[e - 1]);
     // ShowArr(data, n);
     QSort(data, n, s, i - 1);
     QSort(data, n, i + 1, e);
